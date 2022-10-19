0d6806c2810cf25aa8fb0ee45956d55026d64bd5
Dilafdo <mdilanfdo1995@gmail.com>
2022-10-19 20:57:51 +0530

    First commit

---
+++ src/api.js
@@ -0,0 +1,14 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b8d42113aemshaae111d9cfd8c77p1a3945jsn56a60049c672',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+
+export const WEATHER_API_KEY = 'f4446d670b9fe165e0316fb31fad83f0';
+
